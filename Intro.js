948c45a1384c6392ded984666299f665ce4a8d0d
codegrann <omondihills02@gmail.com>
2024-05-09 12:16:01 +0300

    .

--- Intro.js
+++ Intro.js
@@ -1,21 +1,27 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
 
 const myVar = 45;
 
+// http module
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
 
-  // res.write(`<h1>Hello World  ${myVar}</h1>`);
-  // res.write(req.url);
-  try {
+  // URL module
+  /*try {
     let q = url.parse(req.url, true).query;
     let text = q.year + " " + q.month + " " + q.day;
     res.write(text);
   } catch (e) {
     res.write("error");
-  }
-  res.end();
+  }*/
+
+  // fs module
+  fs.readFile("index.html", (err, data) => {
+    res.write(data);
+    res.end();
+  });
 });
 
 server.listen(3000);
