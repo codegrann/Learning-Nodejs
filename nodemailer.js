70a4b9937851663ac0d1e78e7f7341c3e215d0b1
codegrann <omondihills02@gmail.com>
2024-05-20 06:11:12 +0300

    .

--- nodemailer.js
+++ nodemailer.js
@@ -2,12 +2,12 @@ let nodemailer = require("nodemailer");
 
 let transpoter = nodemailer.createTransport({
   service: "gmail",
-  auth: { user: "omondihills02@gmail.com", pass: "reset-1000" },
+  auth: { user: "omondihills02@gmail.com", pass: "React@101" },
 });
 
 let mailOptions = {
   from: "omondihills02@gmail.com",
-  to: "omondihills02@gmail.com",
+  to: "renderer721@gmail.com",
   subject: "Sending Email using Node.js",
   text: "That was easy!",
 };
